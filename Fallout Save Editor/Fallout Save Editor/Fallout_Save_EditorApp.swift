7cfa4611c28a2a4b07c1dd5e84579be952fa88ef
kacollins22 <kacollins@mines.edu>
2024-10-09 09:20:08 -0600

    Created Base XCode App

---
+++ Fallout Save Editor/Fallout Save Editor/Fallout_Save_EditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fallout_Save_EditorApp.swift
+//  Fallout Save Editor
+//
+//  Created by Kyle Collins on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Fallout_Save_EditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
