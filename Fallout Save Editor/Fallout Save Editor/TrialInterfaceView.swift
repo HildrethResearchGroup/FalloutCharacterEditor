30a0afecc5aaed5de524dca0baf82cfc84658b4f
E-L-Cook <ecook@mines.edu>
2024-11-19 12:32:37 -0700

    Most updated GUI

---
+++ Fallout Save Editor/Fallout Save Editor/TrialInterfaceView.swift
@@ -0,0 +1,18 @@
+//
+//  TrialInterfaceView.swift
+//  Fallout Save Editor
+//
+//  Created by Rafiq on 11/5/24.
+//
+
+import SwiftUI
+
+struct TrialInterfaceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TrialInterfaceView()
+}
